c1ecb109bb3c5e3ff5f346d5554d64218db80ca4
Derek White <derekwhite.m@gmail.com>
2022-10-15 21:03:42 -0400

    initial layout setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,15 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
+  devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
   },
   devServer: {
     static: {
-        directory: path.resolve(__dirname, 'dist'),
+      directory: path.resolve(__dirname, 'dist'),
     },
   },
 };
